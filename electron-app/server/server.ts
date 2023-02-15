a72d26a709094105bc0ca27412f97b22d267aab9
Morry <morristskolman@gmail.com>
2023-02-15 11:08:23 -0500

    unlinked backend files

--- electron-app/server/server.ts
+++ electron-app/server/server.ts
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 const PORT = 3000;
-
+``
 /**
  * handle parsing request body
  */
@@ -13,7 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //at homepage - on endpoint main, send main page to front end
-app.get('/routes', (req, res) => {
+
+//req.body will have the serverDir and the serverFile
+  //copy full server from filepath (shell command?
+  //clone the copied server to a renamed server
+  //get the express app obj of both sevrers (might require a shell command)
+  //run mergeTrees and send the finalObj back as a response
+
+
+app.post('/routes', (req, res) => {
   res.json(
     {
       routeName: '/character',
