a8e88495306cdbe2bd38e134e4949b13c512bfe1
Morry <morristskolman@gmail.com>
2023-02-18 16:02:45 -0500

    moved server file

--- electron-app/server_dep_delete/controllers/serverDirPaths.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  processFolder: path.join(__dirname, "../process/"),
-  node_modules: path.join(__dirname, "../process/node_modules"),
-  copiedServer: path.join(__dirname, "../process/copiedServer"),
-  renamedServer: path.join(__dirname, "../process/renamedServer"),
-};
