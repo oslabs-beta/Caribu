dfdc3395179f60d83ee0e488d49c2f73b060d2fd
Justin <jribarich98@gmail.com>
2023-02-25 15:04:25 -0500

    Remade Welcome page with new file drop system and created reducer for node modules

--- electron-app/src/components/WelcomePage.tsx
+++ electron-app/src/components/WelcomePage.tsx
@@ -1,38 +1,62 @@
+import "../../src/index.css";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
-import { fetchRoutes, update_filepath, update_serverpath } from "../slices/viewsSlice";
+import {
+  fetchRoutes,
+  update_filepath,
+  update_serverpath,
+  update_nodepath,
+} from "../slices/viewsSlice";
+import DragDrop from "./DragDrop";
 
 const WelcomePage = (props: object) => {
   const dispatch = useDispatch();
   const views = useSelector((state: RootState) => state.views);
   return (
-    <div>
-      <div className='wp-header'>Welcome. Yup</div>
-      <div className='wp-instructions'>To get started, drag and drop your application folder. Then specify the relative path of your main server file. </div>
-      <div className='form-wrapper'>
-        <form>
-          <input type="file" type="file" id="filepicker" name="fileList" webkitdirectory multiple 
-            onChange={async (e)=> {
-            console.log('WelcomePage file onChange func fired with ', e.target.files[0].path);
-            await dispatch(update_filepath({path: e.target.files[0].path}));
-            console.log('WelcomePage file onChange after dispatch state is', views.filepath);
-            }}/>
-          <input type="text" onChange={async (e)=>{
-            console.log('WelcomePage text onChange func fired with ', e.target.value);
-            await dispatch(update_serverpath({path: e.target.value}));
-            console.log('WelcomePage text onChange after dispatch state is', views.serverpath);
-          }}/>
-          <button onClick={async (e)=>{
-            e.preventDefault();
-            console.log("WelcomePage button onClick fired");
-            await dispatch(fetchRoutes());
-            console.log("WelcomePage button onClick after dispatch state is", views.routes);
-            }}>Submit</button>
+    <div className="welcomePage">
+      <div className="wp-header">Welcome To Caribu</div>
+      <div className="wp-instructions">
+        To get started, drop your file into the dropzone, then simply specify
+        your server files and node modules relative path, and submit!{" "}
+      </div>
+      <div>
+        <DragDrop />
+        <form className="form-wrapper">
+          <input
+            className="serverName-wrapper"
+            type="text"
+            placeholder="Enter server name here"
+            onChange={async (e) => {
+              await dispatch(update_serverpath({ path: e.target.value }));
+            }}
+          />
+          <input
+            className="nodePath-wrapper"
+            type="text"
+            placeholder="Enter node modules path here"
+            onChange={async (e) => {
+              await dispatch(update_nodepath({ path: e.target.value }));
+            }}
+          />
+          <button
+            className="serverSubmit"
+            onClick={async (e) => {
+              // check if server input is empty
+              if (!views.serverpath) {
+                alert("Please specify your servers name first!");
+                return;
+              }
+              e.preventDefault();
+              await dispatch(fetchRoutes());
+            }}
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default WelcomePage;
