919ec2b4050c2eb78dc0ac0f301da327082a7c28
MelodyDuan <melodyy.duan@gmail.com>
2023-03-02 10:46:16 -0500

    rm dist/

--- electron-app/dist/components/metrics/MGraph.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-function MGraph() {
-    return ((0, jsx_runtime_1.jsx)("div", Object.assign({ className: "m-graph" }, { children: "MGraph" })));
-}
-exports.default = MGraph;
-//# sourceMappingURL=MGraph.js.map
